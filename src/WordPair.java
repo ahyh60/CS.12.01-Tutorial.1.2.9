1bf38ac64f93ddc1ff94af210cb450554ff08a8d
ahyh60 <123133761+ahyh60@users.noreply.github.com>
2023-09-20 11:42:20 +0800

    Create WordPair.java

---
+++ src/WordPair.java
@@ -0,0 +1,16 @@
+public class WordPair {
+    private String first;
+    private String second;
+    public WordPair(String f, String s){
+        first = f;
+        second = s;
+    }
+
+    public String getFirst(){
+        return first;
+    }
+
+    public String getSecond(){
+        return second;
+    }
+}
